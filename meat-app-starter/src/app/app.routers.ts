b2e10aec3f6d228e075eaf5717aca49dccdbcd56
Guilherme Ferreira <guilherme12ferreirasouza@gmail.com>
2020-06-04 22:14:00 -0300

    ate aula 17

--- meat-app-starter/src/app/app.routers.ts
+++ meat-app-starter/src/app/app.routers.ts
@@ -1,3 +1,6 @@
+import { ReviewComponent } from './restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { Component } from '@angular/core';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { AboutComponent } from './about/about.component';
@@ -8,5 +11,11 @@ export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'restaurants', component: RestaurantsComponent},
-    {path: 'restaurants/:id', component: RestaurantDetailComponent}
+    {path: 'restaurants/:id', component: RestaurantDetailComponent,
+        children: [
+            {path: '', redirectTo: 'menu', pathMatch: 'full'},
+            {path: 'menu', component: MenuComponent},
+            {path: 'reviews', component: ReviewComponent}
+        ]    
+    }
 ]
---
+++ meat-app-starter/src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-review',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
